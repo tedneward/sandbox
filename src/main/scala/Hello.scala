8e244272c5471a39f402fb17b4047de472483547
Ted Neward <ted@tedneward.com>
2014-12-22 15:55:40 -0800

    Getting crazy

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -2,7 +2,7 @@
  * Scaladocs placeholder so we can see how this would all work
  */
 object Hello {
-  def main(args: Array[String]) : Unit {
-    println("Hello, world")
+  def main(args: Array[String]) : Unit = {
+    System.out.println("Hello, world")
   }
 }
---
+++ testScript.sh
@@ -0,0 +1,4 @@
+echo Lets see how crazy we can get with git in mid-sbt execution
+git checkout gh-pages
+ls * > log.tmp
+git checkout master
