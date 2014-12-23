c0d9cc93199d6639b1249c580cbbd33645f09fee
Ted Neward <ted@tedneward.com>
2014-12-22 16:18:44 -0800

    Forgot that testScript.sh didn't belong on gh-pages branch

--- testScript.sh
+++ testScript.sh
@@ -1,4 +1,15 @@
-echo Lets see how crazy we can get with git in mid-sbt execution
+echo On master
+echo ... stash working changes
+git stash
+
 git checkout gh-pages
-ls * > log.tmp
+echo On gh-pages
+echo ... cleaning out old docs folder
+rm -r docs/api/*
+echo ... copying target/scala-2.11/api/* over to docs/api
+cp -r target/scala-2.11/api/* docs/api
+git add docs/api
+git commit -m "Documentation commit"
+
 git checkout master
+git stash pop
